f8ac4f0de7b7b3ecdd48723721c06500e0577899
Caroline de carvalho Azevedo <carolineazevedo075@gmail.com>
2025-11-03 22:47:48 -0300

    FIX: Move menu mobile para componente separado (BottomNavMobile) no layout

---
+++ fix-utf8.ps1
@@ -0,0 +1,28 @@
+﻿# Mapeamento de correções UTF-8
+ = @{
+    'ConfiguraÃƒÆ.*?Âµes dinÃƒÆ.*?Â¢micas' = 'Configurações dinâmicas'
+    'CustomizaÃƒÆ.*?Âµes' = 'Customizações'
+    'chamadas ÃƒÆ.*?Â ' = 'chamadas à'
+    'sugestÃƒÆ.*?Âµes' = 'sugestões'
+    'usuÃƒÆ.*?Ârio' = 'usuário'
+    'FUNÃƒÆ.*?Â¢O' = 'FUNÇÃO'
+    'pÃƒÆ.*?Âgina' = 'página'
+    'SUGESTÃƒÆ.*?Â¢O' = 'SUGESTÃO'
+    'dinÃƒÆ.*?Â¢micas' = 'dinâmicas'
+    'posiÃƒÆ.*?Â§ÃƒÆ.*?Â£o' = 'posição'
+    'logo ÃƒÆ.*?Â' = 'logo é'
+    'ÃƒÆ.*?ÂÃƒÆ.*?Â¢' = ''
+    'CÃƒÆ.*?Â³d:' = 'Cód:'
+    'O que vocÃƒÆ.*?Âª procura?' = 'O que você procura?'
+    'Ver todos os resultados.*?Â¢' = 'Ver todos os resultados'
+}
+
+ = Get-Content 'components\loja\LojaHeader.tsx' -Raw -Encoding UTF8
+
+foreach ( in .Keys) {
+     = []
+     =  -replace , 
+}
+
+ | Set-Content 'components\loja\LojaHeader.tsx' -Encoding UTF8 -NoNewline
+Write-Host ' Arquivo limpo!'
